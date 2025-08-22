cccc0671295f06c4cd564d50a36dd53336918ddc
Tommaso Redaelli <tommasoredaelli276@gmail.com>
2025-08-22 10:11:49 +0200

    dev

--- src/saferplaces_agent/agent/nodes/tools/saferplaces_api_tools/saferbuildings_tool.py
+++ src/saferplaces_agent/agent/nodes/tools/saferplaces_api_tools/saferbuildings_tool.py
@@ -2,6 +2,7 @@ import os
 import datetime
 from dateutil import relativedelta
 from enum import Enum
+import requests
 
 from typing import Optional, Union, List, Dict, Any
 from pydantic import BaseModel, Field, AliasChoices, field_validator, model_validator
@@ -233,37 +234,6 @@ class SaferBuildingsInputSchema(BaseModel):
 
 # DOC: This is a demo tool to retrieve weather data.
 class SaferBuildingsTool(BaseAgentTool):
-    
-    
-    # DOC: Tool input schema
-    # class InputSchema(BaseModel):
-        
-    #     area: None | str | list[float] = Field(
-    #         title = "Area",
-    #         description = """The area of interest for the weather data. If not specified use None as default.
-    #         It could be a bouning-box defined by [min_x, min_y, max_x, max_y] coordinates provided in EPSG:4326 Coordinate Reference System.
-    #         Otherwise it can be the name of a country, continent, or specific geographic area.""",
-    #         examples=[
-    #             None,
-    #             "Italy",
-    #             "Paris",
-    #             "Continental Spain",
-    #             "Alps",
-    #             [12, 52, 14, 53],
-    #             [-5.5, 35.2, 5.58, 45.10],
-    #         ]
-    #     )
-    #     date: None | str = Field(
-    #         title = "Date",
-    #         description = f"The date of the meteo data provided in UTC-0 YYYY-MM-DD. If not specified use {(datetime.datetime.now() + relativedelta.relativedelta(days=1)).strftime('%Y-%m-%d')} as default.",
-    #         examples = [
-    #             None,
-    #             "2025-01-01",
-    #             "2025-02-01",
-    #             "2025-03-10",
-    #         ],
-    #         default = None
-    #     )
         
     
     # DOC: Initialize the tool with a name, description and args_schema
@@ -292,8 +262,8 @@ class SaferBuildingsTool(BaseAgentTool):
             args_schema = SaferBuildingsInputSchema,
             **kwargs
         )
-        self.execution_confirmed = True  # INFO: This is used to confirm the execution of the tool
-        self.output_confirmed = True    # INFO: There is already the execution_confirmed:True
+        self.execution_confirmed = False
+        self.output_confirmed = True
         
     
     # DOC: Validation rules ( i.e.: valid init and lead time ... ) 
@@ -310,42 +280,6 @@ class SaferBuildingsTool(BaseAgentTool):
     
     # DOC: Inference rules ( i.e.: from location name to bbox ... )
     def _set_args_inference_rules(self) -> dict:
-        
-        def infer_area(**ka):
-            def bounding_box_from_location_name(area):
-                if type(area) is str:
-                    area = utils.ask_llm(
-                        role = 'system',
-                        message = f"""Please provide the bounding box coordinates for the area: {area} with format [min_x, min_y, max_x, max_y] in EPSG:4326 Coordinate Reference System. 
-                        Provide only the coordinates list without any additional text or explanation.""",
-                        eval_output = True
-                    )
-                    self.execution_confirmed = False
-                return area
-            def round_bounding_box(area):
-                if type(area) is list:
-                    precision = 1
-                    area = [
-                        utils.floor_decimals(area[0], precision),
-                        utils.floor_decimals(area[1], precision),
-                        utils.ceil_decimals(area[2], precision),
-                        utils.ceil_decimals(area[3], precision)
-                    ]
-                return area
-            area = bounding_box_from_location_name(ka['area'])
-            area = round_bounding_box(area)
-            return area
-        
-        def infer_date(**ka):
-            if ka['date'] is None:
-                return (datetime.datetime.now() + relativedelta.relativedelta(days=1)).strftime('%Y-%m-%d')
-            return ka['date']
-        
-        
-        # return {
-        #     'area': infer_area,
-        #     'date': infer_date,
-        # }
         return dict()
         
     
@@ -355,17 +289,37 @@ class SaferBuildingsTool(BaseAgentTool):
         /,
         **kwargs: Any,  # dict[str, Any] = None,
     ): 
-        # DOC: this is a dummy execution and provided output
-        # weather_descr = f'It will rain {sum(area) / 4} mm in {area} on {date}'
-        
-        return {
-            "output": "3 edifici allagati",
+        # DOC: Call the SaferBuildings API ...
+        print('\n'*2, '-'*80, '\n')
+
+        api_root_local = "http://localhost:5000" # TEST: only when running locally
+        api_url = f"{os.getenv('SAFERPLACES_API_ROOT')}/processes/safer-buildings-process/execution"
+        payload = { 
+            "inputs": kwargs  | {
+                "token": os.getenv("SAFERPLACES_API_TOKEN"),
+                "user": os.getenv("SAFERPLACES_API_USER"),
+            } | {
+                "wd_thresh": 1.0, # speed-up
+                "out": f"s3://saferplaces.co/SaferPlaces-Agent/dev/saferbuildings_tool/out_{datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y%m%dT%H%M%S')}.gpkg",
+                "summary_on": "subtype",  # TEST: use subtype for summary
+            } | {
+                "debug": True,  # TEST: enable debug mode
+            }
         }
+        print(f"Executing {self.name} with args: {payload}")
+        response = requests.post(api_url, json=payload)
+        print(f"Response status code: {response.status_code} - {response.content}")
+        response = response.json() 
+        # TODO: Check output_code ...
+        
+        print('\n', '-'*80, '\n')
+        
+        return response
         
     
     # DOC: Back to a consisent state
     def _on_tool_end(self):
-        self.execution_confirmed = True
+        self.execution_confirmed = False
         self.output_confirmed = True
         
     
